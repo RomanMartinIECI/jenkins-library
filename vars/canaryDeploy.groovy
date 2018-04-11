84de33fce0ca5cd2146c86773443b553c6861222
RomanMartinIECI <roman_martin@iecisa.com>
2018-04-11 18:20:15 +0200

    Fixed typo

--- vars/canaryDeploy.groovy
+++ vars/canaryDeploy.groovy
@@ -3,5 +3,5 @@
 def call(project, app, version){
     sh "oc new-app -n ${project} --name ${app}-${version} ${app}:${version} -l app=${app},deploymentconfig=${app},hystrix.enabled=true"
     /* sh "oc patch svc/${app} -n ${project} -p '{\"spec\":{\"selector\":{\"app\": \"${app}\", \"deploymentconfig\": null}, \"sessionAffinity\":\"None\"}}'"*/
-    sh "oc set env dc/${app} RESPONSE=Updated ${app} to version ${version}"
+    sh "oc set env dc/${app} RESPONSE=\"Updated ${app} to version ${version}\""
 }
