ad37ed3dd70a0a949d5edc1a45d12a953bd0a3e5
RomanMartinIECI <roman_martin@iecisa.com>
2018-04-12 13:42:31 +0200

    Update rollingDeploy.groovy

--- vars/rollingDeploy.groovy
+++ vars/rollingDeploy.groovy
@@ -4,6 +4,6 @@ def call(project, app, version){
     /* sh "oc new-app -n ${project} --name ${app}-${version} ${app}:${version} -l app=${app},deploymentconfig=${app},hystrix.enabled=true"
     sh "oc patch svc/${app} -n ${project} -p '{\"spec\":{\"selector\":{\"app\": \"${app}\", \"deploymentconfig\": null}, \"sessionAffinity\":\"None\"}}'"
     sh "oc set env dc/${app} RESPONSE=\"Updated ${app} to version ${version}\""*/
-    sh "oc tag ${project}/${app}:${tag} ${project}/${app}:latest"
+    sh "oc tag ${project}/${app}:${version} ${project}/${app}:latest"
     sh "oc rollout latest ${app} -n ${project}"
 }
